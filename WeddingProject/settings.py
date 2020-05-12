986e9835483ea2726e1a7c51010601651d878830
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-12 11:17:53 +0800

    Added More Fixes From StackOverFlow

--- WeddingProject/settings.py
+++ WeddingProject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,11 +122,11 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static',)
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles',)
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static',),
 )
 
