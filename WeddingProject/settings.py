c2e75e42967db27c7a38173425a8b57ec98770c1
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-12 11:08:36 +0800

    Added a weird fix?

--- WeddingProject/settings.py
+++ WeddingProject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static',)
 
 STATIC_URL = '/static/'
 
