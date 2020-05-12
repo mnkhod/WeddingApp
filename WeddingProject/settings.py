178a89f7e18d677124dbc0f0f3574d548bae39c9
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-12 11:11:08 +0800

    Disabled the debug

--- WeddingProject/settings.py
+++ WeddingProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'haq9s#x8%duhtd#ea1z^qjryaoeb(95#k*hubic@r(6l%ntn(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['diplomaweddingwebsite.herokuapp.com']
 
