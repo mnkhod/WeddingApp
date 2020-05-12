1b117e8fc8359a97f9c285a605e4633160d09a4f
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-12 11:04:49 +0800

    Included URLS

--- WeddingProject/urls.py
+++ WeddingProject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from WeddingApp.views import main
+from django.urls import path , include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',main)
+    path('', include('WeddingApp.urls'))
 ]
